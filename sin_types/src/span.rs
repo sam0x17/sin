a981e81f1faacce43e4bbece0a7cfd18ad38989e
Sam Johnson <sam@durosoft.com>
2023-06-20 23:32:21 -0400

    refactor

--- sin_types/src/span.rs
+++ sin_types/src/span.rs
@@ -6,13 +6,13 @@ use proc_macro::Span as Span1;
 use symbol::Symbol;
 
 #[derive(Clone, Copy, Debug)]
-pub enum Span {
-    Sin(Span2),
-    ProcMacro(Span1),
+pub struct Span {
+    span: Span2,
 }
 
-#[derive(Clone, Debug, Copy, Eq, PartialEq, Hash)]
-pub enum Span2 {
+#[derive(Clone, Debug, Copy)]
+enum Span2 {
+    ProcMacro(Span1),
     Local {
         start: usize,
         len: usize,
@@ -24,33 +24,37 @@ pub enum Span2 {
 
 impl From<Span1> for Span {
     fn from(value: Span1) -> Self {
-        Span::ProcMacro(value)
+        Span {
+            span: Span2::ProcMacro(value),
+        }
     }
 }
 
 impl From<&Span1> for Span {
     fn from(value: &Span1) -> Self {
-        Span::ProcMacro(*value)
+        Span {
+            span: Span2::ProcMacro(*value),
+        }
     }
 }
 
 impl From<&Span2> for Span {
     fn from(value: &Span2) -> Self {
-        Span::Sin(value.clone())
+        Span { span: *value }
     }
 }
 
 impl From<Span2> for Span {
     fn from(value: Span2) -> Self {
-        Span::Sin(value.clone())
+        Span { span: value }
     }
 }
 
 impl From<Span> for Span1 {
     fn from(value: Span) -> Self {
-        match value {
-            Span::Sin(Span2::MixedSite) => Span1::mixed_site(),
-            Span::ProcMacro(span) => span,
+        match value.span {
+            Span2::MixedSite => Span1::mixed_site(),
+            Span2::ProcMacro(span) => span,
             _ => Span1::call_site(),
         }
     }
@@ -62,31 +66,35 @@ impl Span {
     /// (call-site hygiene) and other code at the macro call site will be able to refer to them
     /// as well.
     pub fn call_site() -> Span {
-        Span::Sin(Span2::CallSite)
+        Span {
+            span: Span2::CallSite,
+        }
     }
 
-    ///A span that represents macro_rules hygiene, and sometimes resolves at the macro
-    ///definition site (local variables, labels, $crate) and sometimes at the macro call site
-    ///(everything else). The span location is taken from the call-site.
+    /// A span that represents macro_rules hygiene, and sometimes resolves at the macro
+    /// definition site (local variables, labels, $crate) and sometimes at the macro call site
+    /// (everything else). The span location is taken from the call-site.
     pub fn mixed_site() -> Span {
-        Span::Sin(Span2::MixedSite)
+        Span {
+            span: Span2::MixedSite,
+        }
     }
 
     /// Returns the source text behind a span. This preserves the original source code,
     /// including spaces and comments.
     ///
     /// [`None`] is returned if the backing is a [`proc_macro::Span`] and the span itself does
-    /// not refer to real source code or if the underlying, or if this is a [`mixed_site`](`Span::mixed_site`) or
+    /// not refer to real source code or if this is a [`mixed_site`](`Span::mixed_site`) or
     /// [`call_site`](`Span::call_site`) span.
     pub fn source_text(&self) -> Option<Symbol> {
-        match self {
-            Span::Sin(Span2::Local {
+        match self.span {
+            Span2::Local {
                 start,
                 len,
                 source_text,
-            }) => Some(source_text[*start..(start + len)].into()),
-            Span::Sin(Span2::MixedSite | Span2::CallSite) => None,
-            Span::ProcMacro(span) => match span.source_text() {
+            } => Some(source_text[start..(start + len)].into()),
+            Span2::MixedSite | Span2::CallSite => None,
+            Span2::ProcMacro(span) => match span.source_text() {
                 Some(source_text) => Some(source_text.into()),
                 None => None,
             },
